eb6ab987b55a95c2f6896b01ed90145fc949ce50
Gavin Dent <gdent@uk.ibm.com>
2016-03-05 16:05:50 +0000

    Modified files for openShift

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
     res.status(200).sendFile(__dirname + '/client/index.html');
 });
 
-app.listen(3000, function () {
-    console.log('App listening on port 3000');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function(){
+    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
